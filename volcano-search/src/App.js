9816aefb902ba643f810962e7003d67a2ba25161
brandon <brandon.roques13@gmail.com>
2023-03-07 15:40:22 -0500

    I am bad at commitment

--- volcano-search/src/App.js
+++ volcano-search/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        {/* <Route path='/' element='the file for the map'/> */}
+        {/* <Route path='/volcano/:someID' element='the path for a clicked volcano'/> */}
+      </Routes>
     </div>
   );
 }
