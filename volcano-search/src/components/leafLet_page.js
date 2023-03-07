48d9b423e65ba9a174ae4f2a609df47899a5c538
brandon <brandon.roques13@gmail.com>
2023-03-07 18:03:40 -0500

    Put the marker and popup on the map

--- volcano-search/src/components/leafLet_page.js
+++ volcano-search/src/components/leafLet_page.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
-import './leaflet_styling.css'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./app.css";
+
 
 const LeafLet = () => {
-  const position = [51.505, -0.09];
   return (
-    <div>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <div position={position}>
-          <div className='map'>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </div>
-        </div>
-      </MapContainer>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   );
 };
 
