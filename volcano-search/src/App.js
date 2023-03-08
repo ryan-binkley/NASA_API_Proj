15beb31a8e1c43e4ace749064e0aeac0946acb52
IzykPringle <ijpringl@mtu.edu>
2023-03-08 13:33:52 -0700

    add About page. Change header to have nav buttons.

--- volcano-search/src/App.js
+++ volcano-search/src/App.js
@@ -4,6 +4,7 @@ import Volcano from './components/volcano.js'
 import LeafLet from './components/leafLet_page';
 import React, { useState } from 'react';
 import HeaderComponent from './components/header_Component/header_Component';
+import AboutPageComponent from './components/about_page_component/about_page_component';
 
 
 export const VolcanoContext = React.createContext([]);
@@ -12,11 +13,12 @@ export const VolcanoContext = React.createContext([]);
 function App() {
 
   const [volcano, setVolcano] = useState('volcano state, changes on button click');
-  
+
   return (
     <div className="App">
       <VolcanoContext.Provider value={{ volcano, setVolcano }}>
         <HeaderComponent />
+        <AboutPageComponent/>
       </VolcanoContext.Provider>
 
 
--- volcano-search/src/components/header_Component/header_Component.js
+++ volcano-search/src/components/header_Component/header_Component.js
@@ -1,10 +1,10 @@
 import './headerComponent.css';
 import React, { useState } from 'react';
-import {VolcanoContext} from '../../App.js';
+import { VolcanoContext } from '../../App.js';
 
 function HeaderComponent() {
 
-    const {volcano, setVolcano} = React.useContext(VolcanoContext);
+    const { volcano, setVolcano } = React.useContext(VolcanoContext);
     const [userInput, setUserInput] = useState('');
 
     const handleChange = (event) => {
@@ -15,7 +15,13 @@ function HeaderComponent() {
     return (
         <>
             <div id='wrapper'>
-                <img id='logo' onClick={() => setVolcano('')} src='./Volcano_Logo.png' alt='Volcano Logo'></img>
+                <div id='navPages'>
+                    <img id='logo' onClick={() => setVolcano('')} src='./Volcano_Logo.png' alt='Volcano Logo'></img>
+                    <div id='navButtons'>
+                        <button onClick={() => setVolcano('')}>Home</button>
+                        <button onClick={() => setVolcano('AboutPage')}>About</button>
+                    </div>
+                </div>
                 <h2 id='title'>Fantastic Volcanoes and Where to Find Them</h2>
                 <div id='search'> {volcano}
                     <input type='text' onChange={handleChange} placeholder='Search...' />
